0cc6a4bdb85fd49929789ad89816153237ba54da
dilli <dilliraj.116@gmail.com>
2024-11-16 09:31:04 -0800

    Successful final script

---
+++ Final/main.tf
@@ -0,0 +1,157 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg1"
+  location = "westus2"
+}
+
+# SSH Public Key
+resource "azurerm_ssh_public_key" "ssh" {
+  name                = "ssh_key"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  public_key          = file("/home/dilli/.ssh/id_rsa.pub")
+}
+# Virtual Network 1
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = ["10.5.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.5.2.0/24"]
+}
+
+# vnet 2
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "vnet2"
+  address_space       = ["10.15.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet2.name
+  address_prefixes     = ["10.15.2.0/24"]
+}
+# Network Peering
+resource "azurerm_virtual_network_peering" "vnet1-to-vnet2" {
+  name                      = "vnet1-to-vnet2"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+  allow_virtual_network_access = true
+}
+
+resource "azurerm_virtual_network_peering" "vnet2-to-vnet1" {
+  name                      = "vnet2-to-vnet1"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+  allow_virtual_network_access = true
+}
+# Network Interface for VM1
+resource "azurerm_network_interface" "vm1_nic" {
+  name                = "vm1-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm1_public_ip.id
+  }
+}
+
+# Network Interface for VM2
+resource "azurerm_network_interface" "vm2_nic" {
+  name                = "vm2-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet2.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_public_ip" "vm1_public_ip" {
+  name                = "vm1-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"  # or "Dynamic"
+  sku                  = "Standard"  # or "Basic" depending on your needs
+}
+# Virtual Machine 1 with Public IP
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2"  # Updated to a different available VM size
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm1_nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = azurerm_ssh_public_key.ssh.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+# Virtual Machine 2 with Private IP
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "vm2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2"  # Updated to a different available VM size
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm2_nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = azurerm_ssh_public_key.ssh.public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+# Output variables for IP addresses
+output "vm1_public_ip" {
+  value       = azurerm_public_ip.vm1_public_ip.ip_address
+  description = "Public IP address of VM1"
+}
+
+output "vm2_private_ip" {
+  value       = azurerm_network_interface.vm2_nic.ip_configuration[0].private_ip_address
+  description = "Private IP address of VM2"
+}
+
